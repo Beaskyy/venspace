9025a0f44098fbd5c8247fa0acda2b52b4c1af7d
Beaskyy <afeez.akintobi@yahoo.com>
2025-01-10 20:52:13 +0100

    update

---
+++ app/gift-cards/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { CreativeIdea } from '@/components/creative-idea';
+import { Footer } from '@/components/footer';
+import { GiftCardHero } from '@/components/gift-card-hero';
+import { HowGiftCardWorks } from '@/components/how-gift-cards-work';
+import { SearchHeader } from '@/components/search-header';
+import { VenspaceGiftCards } from '@/components/venspace-gift-cards';
+
+const GiftCards = () => {
+  return (
+    <div>
+      <SearchHeader />
+      <GiftCardHero />
+      <HowGiftCardWorks />
+      <VenspaceGiftCards />
+      <CreativeIdea />
+      <Footer />
+    </div>
+  );
+}
+
+export default GiftCards
