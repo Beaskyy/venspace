068f3c90d512b6bdbe87eff2e3bc26cb85a42293
Beaskyy <afeez.akintobi@yahoo.com>
2025-01-13 13:40:15 +0100

    bookings-header

--- components/bookings-header.tsx
+++ components/bookings-header.tsx
@@ -2,22 +2,88 @@ import { AlignJustify, ChevronDown } from "lucide-react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { Input } from "./ui/input";
+import { Navigator } from "./navigator";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { submenuItems } from "@/lib/data";
+import { cn } from "@/lib/utils";
 
 export const BookingsHeader = () => {
   return (
     <div
-      className="flex md:flex-row flex-col gap-6 justify-between py-4 lg:px-[72px] px-4"
+      className="flex gap-6 justify-between py-4 lg:px-[72px] px-4"
       style={{ boxShadow: "0px 2px 19px 0px #0000000D" }}
     >
-      <Link href="/" className="flex items-center gap-6 z-50">
-        <Image src="/logo.png" alt="logo" width={64} height={64} />
-        <div className="flex items-center gap-1 cursor-pointer">
-          <p className="hidden lg:block text-[#001224]">Browse Spaces</p>
-          <ChevronDown className="text-[#001224] size-[18px]" />
-        </div>
-      </Link>
+      <div className="flex md:gap-4 gap-2">
+        <Link href="/" className="flex items-center gap-6 z-50">
+          <div className="relative md:w-16 md:h-16 w-[43px] h-[43px]">
+            <Image src="/logo.png" alt="logo" fill className="absolute" />
+          </div>
+        </Link>
+        <DropdownMenu>
+          {/* Main Trigger */}
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center gap-1 cursor-pointer">
+              <p className="hidden lg:block text-[#001224]">Browse Spaces</p>
+              <ChevronDown className="text-[#001224] size-[18px]" />
+            </div>
+          </DropdownMenuTrigger>
+
+          {/* Main Menu */}
+          <DropdownMenuContent
+            className="bg-white w-[165px] shadow-lg py-2 rounded-xl"
+            sideOffset={8}
+            align="start"
+          >
+            {Object.keys(submenuItems).map((item) => (
+              <DropdownMenuSub key={item}>
+                {/* Main Menu Item */}
+                <DropdownMenuSubTrigger className="flex items-center justify-between px-4 py-2 text-[#001224] text-base font-medium hover:bg-gray-100 cursor-pointer">
+                  {item}
+                </DropdownMenuSubTrigger>
+
+                {/* Submenu */}
+                <DropdownMenuSubContent
+                  className={cn(
+                    "bg-white min-w-[165px] shadow-lg py-2 rounded-none rounded-tr-xl rounded-br-xl"
+                  )}
+                  sideOffset={8}
+                >
+                  {submenuItems[item].map((submenuItem, index) => (
+                    <DropdownMenuItem asChild key={index}>
+                      <Link
+                        href={`/${item
+                          .toLowerCase()
+                          .replace(/\s/g, "-")}/${submenuItem
+                          .toLowerCase()
+                          .replace(/\s/g, "-")}`}
+                        className={`block px-4 py-2 text-base font-medium whitespace-nowrap ${
+                          submenuItem === "See All"
+                            ? "text-[#F44363]"
+                            : "text-[#001224]"
+                        } hover:bg-gray-100`}
+                      >
+                        {submenuItem}
+                      </Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div
-        className="flex items-center gap-12 md:w-[365px] w-full h-[60px] border border-[#1A1A1A14] rounded-[64px] py-1.5 pl-8 pr-2"
+        className="hidden md:flex items-center gap-12 md:w-[365px] w-full h-[60px] border border-[#1A1A1A14] rounded-[64px] py-1.5 pl-8 pr-2"
         style={{ boxShadow: "0 4px 15px 0 rgba(0, 0, 0, 0.06)" }}
       >
         <div className="flex gap-x-4 md:w-[229px] w-full">
@@ -59,7 +125,7 @@ export const BookingsHeader = () => {
         </div>
       </div>
       <div className="lg:hidden flex justify-center items-center z-50">
-        <AlignJustify className="text-white size-6" />
+        <Navigator color="[#434242]" />
       </div>
     </div>
   );
