079f1d06c2e409459bf99b9509e020759c4719a1
Beaskyy <afeez.akintobi@yahoo.com>
2025-01-19 12:00:19 +0100

    update

--- app/profile/membership/page.tsx
+++ app/profile/membership/page.tsx
@@ -5,18 +5,33 @@ import { Footer } from "@/components/footer";
 import { MembershipPlan } from "@/components/membership-plan";
 import { SearchHeader } from "@/components/search-header";
 import { MembershipForm } from "@/components/membership-form";
+import { MembershipReview } from "@/components/membership-review";
 
 const Membership = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedId, setSelectedId] = useState<number>(0);
+  const [selectedAmount, setSelectedAmount] = useState(0)
   return (
     <div>
       <SearchHeader />
       <div className="flex flex-col gap-12 lg:px-[72px] my-10 lg:pt-0 md:p-8 p-4">
         {currentPage === 0 ? (
-          <MembershipPlan setCurrentPage={setCurrentPage} setSelectedId={setSelectedId} />
+          <MembershipPlan
+            setCurrentPage={setCurrentPage}
+            setSelectedId={setSelectedId}
+            setSelectedAmount={setSelectedAmount}
+          />
         ) : currentPage === 1 ? (
-          <MembershipForm setCurrentPage={setCurrentPage} selectedId={selectedId} />
+          <MembershipForm
+            setCurrentPage={setCurrentPage}
+            selectedId={selectedId}
+          />
+        ) : currentPage === 2 ? (
+          <MembershipReview
+            setCurrentPage={setCurrentPage}
+            selectedId={selectedId}
+            selectedAmount={selectedAmount}
+          />
         ) : (
           "beasky"
         )}
